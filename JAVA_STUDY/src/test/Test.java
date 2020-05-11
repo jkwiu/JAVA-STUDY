6ed23bf86b41fa462b761d647c6097bc172cedd3
jk <sizzflyer@gmail.com>
2020-05-12 02:12:11 +0900

    ch7 ing

--- JAVA_STUDY/src/test/Test.java
+++ JAVA_STUDY/src/test/Test.java
@@ -1,25 +1,38 @@
 package test;
 
-class Document {
-    static int count = 0;
-    String name;
+class Tv {
+    boolean power;
+    int channel;
 
-    Document(){
-        this("제목없음" + ++count);
+    void power(){
+        power = !power;
     }
+    void channelUp(){
+        ++channel;
+    }
+    void channelDown(){
+        --channel;
+    }
+    
+}
 
-    Document(String name){
-        this.name = name;
-        System.out.println("문서 " + this.name + "가 생성되었습니다.");
+class CaptionTv extends Tv {
+    boolean caption;
+    void displayCaption(String text){
+        if(caption){
+            System.out.println(text);
+        }
     }
 }
 
 class Test {
     public static void main(String[] args) {
-        Document t1 = new Document();
-        Document t2 = new Document();
-        Document t3 = new Document();
-        Document t4 = new Document();
-        Document t5 = new Document();
+        CaptionTv ctv = new CaptionTv();
+        ctv.channel = 10;
+        ctv.channelUp();
+        System.out.println(ctv.channel);
+        ctv.displayCaption("Hello World");
+        ctv.caption = true;
+        ctv.displayCaption("Hello World");
     }
 }
